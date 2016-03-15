1c4a8ee7b209880c6388ab9642ef54ef638d0e93
Jerry Mooon and Philip Kuryloski <pair+jerry+philip@pivotal.io>
2016-03-14 18:01:14 -0700

    Add some data

--- test_fixture.sql
+++ test_fixture.sql
@@ -1,6 +1,9 @@
+DROP TABLE IF EXISTS t_config, t_frh_calc, t_fsh_calc, t_gas_temp;
+
 CREATE TABLE IF NOT EXISTS t_config (
 	i_div character varying(30),
-	i_type character varying(30)
+	i_type character varying(30),
+	i_att1 character varying(100)
 );
 
 CREATE TABLE IF NOT EXISTS t_frh_calc (
@@ -95,3 +98,16 @@ CREATE TABLE IF NOT EXISTS t_gas_temp (
 	n_ltrh_inlet real,
 	n_eco2_inlet real
 );
+
+INSERT INTO t_config
+	(i_div, i_type, i_att1) VALUES
+	('F.S.H', 'Limit Line', '637');
+
+INSERT INTO t_gas_temp
+	(d_timestamp, n_mw, n_frh_inlet, n_fsh_inlet, n_ltrh_inlet, n_eco2_inlet) VALUES
+	('2016-03-01 00:00:00', 497, 1037.1, 932.2, 801.5, 570.9);
+
+INSERT INTO t_frh_calc
+	(i_div, d_timestamp, i_tube_no, n_mw, n_att1, n_att2, n_att3, n_att4, n_att5, n_att6, n_att7, n_att8, n_att9, n_att10, n_att11, n_att12, n_att13, n_att14, n_att15, n_att16, n_att17, n_att18, n_att19, n_att20, n_att21, n_att22, n_att23) VALUES
+	('F.R.H_3', '2016-03-01 00:00:00', 1, 497, 690, 691,  692,    693,    694,    695,    696,    697,    698,    699,     700,     701,     702,     703,     704,     705,     706,     707,     708,     709,     710,     711,     712),
+	('F.R.H_5', '2016-03-01 00:00:00', 1, 497, 590, 591,  592,    593,    594,    595,    596,    597,    598,    599,     600,     601,     602,     603,     604,     605,     606,     607,     608,     609,     610,     611,     612);
